f04926733388af12ecc112c605547a392e530149
jeffjacobsva <jeffrey.jacobs1@va.gov>
2025-04-18 13:16:38 -1000

    Updated comment on port

--- moto/run-moto.sh
+++ moto/run-moto.sh
@@ -14,7 +14,7 @@ python -m pip install moto[all] flask flask_cors
 # Set the LOG_LEVEL environment variable for debugging (optional)
 export LOG_LEVEL=DEBUG
 
-# Run moto_server on port 5000
+# Run moto_server on port 4566
 moto_server -p 4566
 
 # Deactivate the virtual environment when done (optional)
